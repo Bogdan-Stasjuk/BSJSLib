73f666542b4540d8c3d5361384104fa6fedc59a1
Bogdan Stasjuk <Bogdan.Stasjuk@gmail.com>
2015-11-25 01:44:55 +0200

    Added BSNotification

---
+++ BSNotification.js
@@ -0,0 +1,31 @@
+/*
+BSNotification start
+ */
+
+BSNotification = {
+	/**
+	 * Creates desktop notification
+	 * @param  {String} title
+	 * @param  {String} body Message
+	 * @param  {Image} image Icon
+	 * @return {void}
+	 */
+	desktop: function(title, body, image) {
+		if (Notification.permission !== "granted") {
+			Notification.requestPermission();
+		}
+
+		var notification = new Notification(title, {
+			icon: image,
+			body: body,
+		});
+
+		notification.onclick = function() {
+			window.focus();
+		};
+	}
+}
+
+/*
+BSNotification end
+ */
