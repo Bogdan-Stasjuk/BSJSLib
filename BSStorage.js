c0686e46e6da7e0fe3f407a70b60b8fbb7afebfa
Bogdan Stasiuk <Bogdan.Stasjuk@gmail.com>
2018-01-08 22:50:54 +0200

    Add clear func

--- BSStorage.js
+++ BSStorage.js
@@ -1,16 +1,19 @@
 BSStorage = {
-	set: function(value, completion) {
-		chrome.storage.sync.set({
-			key: value
-		}, completion);
-	},
-	get: function(completion) {
-		chrome.storage.sync.get({
-			key: BSStorageDefaults.get()
-		}, function(items) {
-			var value = items.key;
-			console.logx("Did get values: " + value);
-			completion(value);
-		});
-	}
+    set: function(value, completion) {
+        chrome.storage.sync.set({
+            key: value
+        }, completion);
+    },
+    get: function(completion) {
+        chrome.storage.sync.get({
+            key: BSStorageDefaults.get()
+        }, function(items) {
+            var value = items.key;
+            console.logx("Did get values: " + value);
+            completion(value);
+        });
+    },
+    clear: function() {
+        chrome.storage.sync.clear();
+    }
 }
